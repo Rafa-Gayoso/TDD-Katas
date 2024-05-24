e413ca41036e90b582a8d58194a5d667c5b0ca59
Rafa Gayoso <gayoso0597@gmail.com>
2024-05-24 19:27:38 +0200

    ✅return_number_when_only_one_number_is_provided

--- StringCalculator/StringCalculator.Application.UnitTest/CalculatorShould.cs
+++ StringCalculator/StringCalculator.Application.UnitTest/CalculatorShould.cs
@@ -16,4 +16,12 @@ public class Tests
         var result = calculator.Add("");
         result.Should().Be(0);
     }
+    
+    [Test]
+    public void return_number_when_only_one_number_is_provided()
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add("1");
+        result.Should().Be(1);
+    }
 }
