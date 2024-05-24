2bae2b824dc677e54cb2823ebfd90425f9aaf446
Rafa Gayoso <gayoso0597@gmail.com>
2024-05-24 19:38:15 +0200

    ✅return_sum_of_two_numbers

--- StringCalculator/StringCalculator.Application.UnitTest/CalculatorShould.cs
+++ StringCalculator/StringCalculator.Application.UnitTest/CalculatorShould.cs
@@ -4,24 +4,32 @@ namespace StringCalculator.Application.UnitTest;
 
 public class Tests
 {
+    private Calculator _calculator;
+
     [SetUp]
     public void Setup()
     {
+        _calculator = new Calculator();
     }
 
     [Test]
     public void return_zero_when_no_numbers_were_provided()
     {
-        var calculator = new Calculator();
-        var result = calculator.Add("");
+        var result = _calculator.Add("");
         result.Should().Be(0);
     }
     
     [Test]
     public void return_number_when_only_one_number_is_provided()
     {
-        var calculator = new Calculator();
-        var result = calculator.Add("1");
+        var result = _calculator.Add("1 ");
         result.Should().Be(1);
+    }
+    
+    [Test]
+    public void return_sum_of_two_numbers()
+    {
+        var result = _calculator.Add("2,3");
+        result.Should().Be(5);
     }
 }
--- StringCalculator/StringCalculator.Application/Calculator.cs
+++ StringCalculator/StringCalculator.Application/Calculator.cs
@@ -4,8 +4,13 @@ public class Calculator
 {
     public int Add(string numbers)
     {
-        if (string.IsNullOrEmpty(numbers)) return 0;   
+        if (string.IsNullOrEmpty(numbers)) return 0;
         
-        return int.Parse(numbers);
+        var splittedNumbers = numbers.Split(',', StringSplitOptions.RemoveEmptyEntries); 
+        
+        if (splittedNumbers.Length == 1) return int.Parse(splittedNumbers[0]);
+
+        var sum = int.Parse(splittedNumbers[0]) + int.Parse(splittedNumbers[1]);
+        return sum;
     }
 }
